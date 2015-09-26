1dcff049b043b4aba0608e85602bc142ad47218b
Todd Beckman <toddbeckman1@gmail.com>
2015-09-26 17:24:19 -0600

    allow search for name or id

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,13 +1,27 @@
 var get=GET();
 var mons = {
-    "bulbasaur":{"name":"Bulbasaur"},
-    "ivysaur":{"name":"Ivysaur"},
-    "venusaur":{"name":"Venusaur"}
+    "missingno.":0,
+    "bulbasaur":1,
+    "ivysaur":2,
+    "venusaur":3
 }
-if (get['mon'] !== undefined && mons[get['mon'].toLowerCase()] === undefined) {
-    window.location.href=window.location.href.split("?")[0];
-}
-var mon = mons[get['mon'].toLowerCase()];
-window.onload = function() {
-    document.getElementById("monname").innerHTML = mon.name;
+var ids=[
+    {"name":"MissingNo."},
+    {"name":"Bulbasaur"},
+    {"name":"Ivysaur"},
+    {"name":"Venusaur"}
+];
+if (get['mon'] !== undefined) {
+    var g=get['mon'].toLowerCase(), g2 = parseInt(g);
+    console.log("found " + g + " and " + g2);
+    if ((isNaN(g2) || g2<0 || ids.length <= g2)
+    &&  mons[g] === undefined) {
+        window.location.href=window.location.href.split("?")[0];
+    }
+    var id = isNaN(g2) ? mons[g] : g2;
+    console.log("found id " + id);
+    var mon = ids[id];
+    window.onload = function() {
+        document.getElementById("monname").innerHTML = "#" + id + ": " + mon.name;
+    }
 }
