f4c9ba16435cc420417b995d4ef80019274bad72
Todd Beckman <toddbeckman1@gmail.com>
2015-10-17 15:26:13 -0600

    updated some things

--- db/game.js
+++ db/game.js
@@ -1,51 +1,97 @@
 var game = {
-    language : Language.English,
-    teams : [],
-    mode : Mode.Rotation,
-    numteams : 2,
-    playerlose: function (team) {
-        this.teams.splice(this.teams.indexOf(this), 1);
-        this.numteams--;
-        if (this.numteams == 1) {
-            this.endGame();
-        }
-    },
-    endGame : function () {
-        game.write(teams[0].name + " is the winner!");
+  language : Language.English,
+  teams : [],
+  mode : Mode.Rotation,
+  numteams : 2,
+  eventID : {
+    T0:{
+      PreviewStart:   0,
+      T0_PreviewEnd:  1,
+      T0_OrderMons:   2,
+      T0_SendOut:     3
     }
+    EOT:{
+      WeatherDamage:  0,
+      StatusDamage:   1,
+      GradualHeal:    2,
+      GradualDamage:  3,
+      CureStatus:     4,
+      GetStatus:      5,
+      PassiveAbility: 6,
+      SendOutMon:     7,
+      FormChange:     8,
+      StatCountdown:  9
+    }
+    END:{
+      END_PlayerLose: 0,
+      END_GameOver:   1
+    }
+  },
+  T0:[],
+  EOT:[],
+  END:[],
+  weather : require("weather"),
+  addEvent:function(arr, priority, iterations, run, args) {
+    arr[priority] = {i:iterations,r:run,a:args};
+  },
+  runEvent:function(arr) {
+    for (var i = 0; i < arr.length; i++) {
+      arr[i].i--;
+      //  Exhausted iteration count
+      if (arr[i].i == -1) {
+        arr.splice(i, 1);
+      }
+      else {
+        //  Allow event to exhaust itself
+        //  while it is being run
+        if (call(arr[i].r, arr[i].args)) {
+          arr.slice(i, 1);
+        }
+      }
+    }
+  }
+  playerlose: function (team) {
+    this.teams.splice(this.teams.indexOf(team), 1);
+    this.numteams--;
+    if (this.numteams == 1) {
+      this.endGame();
+    }
+  }
 };
 
+
+
 var Mode = {
-    Single : {
-        activeslots:1,
-        benchslots:0,
-        allowitems : false
-    },
-    Double : {
-        activeslots:2,
-        benchslots:0
-        allowitems : false
-    },
-    Triple : {
-        activeslots:3,
-        benchslots:0
-        allowitems : false
-    },
-    Rotation : {
-        activeslots:1,
-        benchslots:2
-        allowitems : false
-    },
-    /*  Potential use
-    TCG : {
-        activeslots:1,
-        benchslots:5
-        allowitems : true
-    },*/
-    /*  Potential use
-    Defender : {
-        activeslots:2,
-        benchslots:1
-        allowitems : false
-    }*/
+  Single : {
+      activeslots:1,
+      benchslots:0,
+      allowitems : false
+  },
+  Double : {
+      activeslots:2,
+      benchslots:0
+      allowitems : false
+  },
+  Triple : {
+      activeslots:3,
+      benchslots:0
+      allowitems : false
+  },
+  Rotation : {
+      activeslots:1,
+      benchslots:2
+      allowitems : false
+  },
+  /*  Potential use
+  TCG : {
+      activeslots:1,
+      benchslots:5
+      allowitems : true
+  },*/
+  /*  Potential use
+  Defender : {
+      activeslots:2,
+      benchslots:1
+      allowitems : false
+  }*/
 };
--- db/weather/weather.js
+++ db/weather/weather.js
@@ -1,79 +1,4 @@
-var WeatherConditions = {
-    "None" : 0,
-    "Rain" : 1,
-    "Sun" : 2,
-    "Sand" : 3,
-    "Hail" : 4,
-    "Heavy Rain" : 5,
-    "Harsh Sun" : 6,
-    "Strong Wind" : 7
-};
-
-//  TODO
-var WeatherDisplay = [
-    ["None"],
-    ["Rain"],
-    ["Sun"],
-    ["Sand"],
-    ["Hail"],
-    ["Heavy Rain"],
-    ["Harsh Sun"],
-    ["Strong Wind"]
-];
-//  TODO
-var WeatherStartMessage = [
-    [],
-    ["It started to rain!"],
-    ["The sunlight turned harsh!"],
-    ["A sandstorm kicked up!"],
-    ["It started to hail!"],
-    ["A heavy rain began to fall!"],
-    ["The sunlight turned extremely harsh!"],
-    ["A mysterious air current is protecting Flying Pokémon!"]
-];
-//  TODO
-var WeatherContinueMessage = [
-    [],
-    ["Rain continues to fail."],
-    ["The sunlight is strong."],
-    ["The sandstorm rages."],
-    ["Hail continues to fall."],
-    ["The heavy rain persists."],
-    ["The extremely harsh sunlight persists."],
-    ["The mysterious air current persists."]
-];
-//  TODO = 
-var WeatherEndMEssage = [
-    [],
-    ["The rain stopped."],
-    ["The sunlight faded."],
-    ["The sandstorm subsided."],
-    ["The hail stopped."],
-    ["The heavy rain is lifted."],
-    ["The harsh sunlight faded."],
-    ["The mysterious air current dissipated."]
-];
-//  TODO
-var WeatherDamageMessage = [
-    [],[],[],
-    [" is buffeted by the sand."],
-    [" is buffeted by the hail."],
-    [],[],[]
-];
-//  TODO
-var WeatherPreventMoveMessage = [
-    [],[],[],[],[],
-    ["The Fire-type attack fizzled out in the heavy rain!"],
-    ["The Water-type attack evaporated in the harsh sunlight!"],
-    []
-];
-//  TODO
-var WeatherPreventChangeMesage = [
-    [],[],[],[],[],
-    ["There is no relief from this heavy rain!"],
-    ["The extremely harsh sunlight was not lessened at all!"],
-    ["The mysterious air current blows regardless."]
-];
+var type=require("type");
 
 var weather = {
     current : 0,    //  Not to be called directly; use weather.flag()
@@ -83,48 +8,46 @@ var weather = {
         if (this.suppressed) {
             return 0;
         }
-        return WeatherObjects[this.current].flag
+        return this.data[this.current].flag
     },
     set : function (to) {
-        if (WeatherObjects[to].priority < WeatherObjects[this.current].priority) {
-            game.write(WeatherPreventChangeMessage[this.current][game.language]);
+        if (this.data[to].priority < this.data[this.current].priority) {
+            game.write(this.PreventChangeMessage[this.current][game.language]);
         }
         else {
             this.current = to;
-            game.write(WeatherBeginMessage[to][game.language]);
+            game.write(this.BeginMessage[to][game.language]);
         }
     },
     MultiplyEvent : function (e, user, move, mtype) {
         var name = "Multiply" + e;
-        if (WeatherObjects[this.current][name] != undefined) {
-            return WeatherObjects[this.current][name](user, move);
+        if (this.data[this.current][name] !== undefined) {
+            return this.data[this.current][name](user, move);
         }
         return 1;
     },
     EOT_WeatherDamage : function (user) {
-        if (WeatherObjects[this.current].EOT_WeatherDamage != undefined) {
-            WeatherObjects[this.current].EOT_WeatherDamage(user);
+        if (this.data[this.current].EOT_WeatherDamage !== undefined) {
+            this.data[this.current].EOT_WeatherDamage(user);
         }
     },
     BeforeMoveUsed : function (user, move) {
-        if (WeatherObjects[this.current].BeforeMoveUsed != undefined) {
-            if (!WeatherObjects[this.current].BeforeMoveUsed(user, move)) {
-                game.write(WeatherPreventMoveMessage[this.current][game.language]);
+        if (this.data[this.current].BeforeMoveUsed !== undefined) {
+            if (!this.data[this.current].BeforeMoveUsed(user, move)) {
+                game.write(this.PreventMoveMessage[this.current][game.language]);
                 return false;
             }
         }
         return true;
     },
     GetTypeEffectiveness : function (user, move, mtype) {
-        if (WeatherObjects[this.current].GetTypeEffectiveness != undefined) {
-            return WeatherObjects[this.current]
+        if (this.data[this.current].GetTypeEffectiveness !== undefined) {
+            return this.data[this.current]
                 .GetTypeEffectiveness(user, move, mtype);
         }
         return false;
-    }
-}
-
-var WeatherObjects = [
+    },
+    data = [
     {   //  None
         flag : 0,
         priority : 0
@@ -172,7 +95,7 @@ var WeatherObjects = [
             ||  user.hasType(8)
             ||  !user.runBeforeEvent("SandDamage")) {
                 user.takeResidualDamage(user.stats[0] / 16,
-                    user.name() + WeatherDamageMessage[3][game.language]);
+                    user.name() + this.DamageMessage[3][game.language]);
             }
             return 1;
         }
@@ -186,7 +109,7 @@ var WeatherObjects = [
             ||  user.hasType(8)
             ||  !user.runBeforeEvent("HailDamage")) {
                 user.takeResidualDamage(user.stats[0] / 16,
-                    user.name() + WeatherDamageMessage[4][game.language]);
+                    user.name() + this.DamageMessage[4][game.language]);
             }
             return 1;
         }
@@ -222,10 +145,87 @@ var WeatherObjects = [
         priority : 1,
         GetTypeEffectiveness : function (user, move, source, target, mtype) {
             //  Neutralize weaknesses to Flying
-            if (TypeEffectiveness[mtype][2] == 4) {
+            if (type.effectiveness[mtype][2] == 4) {
                 return 1;
             }
             return false;
         }
-    }
+    },
+    Conditions = {
+        "None" : 0,
+        "Rain" : 1,
+        "Sun" : 2,
+        "Sand" : 3,
+        "Hail" : 4,
+        "Heavy Rain" : 5,
+        "Harsh Sun" : 6,
+        "Strong Wind" : 7
+    };
+    //  TODO
+    Display:[
+        ["None"],
+        ["Rain"],
+        ["Sun"],
+        ["Sand"],
+        ["Hail"],
+        ["Heavy Rain"],
+        ["Harsh Sun"],
+        ["Strong Wind"]
+    ];
+    //  TODO
+    StartMessage:[
+        [],
+        ["It started to rain!"],
+        ["The sunlight turned harsh!"],
+        ["A sandstorm kicked up!"],
+        ["It started to hail!"],
+        ["A heavy rain began to fall!"],
+        ["The sunlight turned extremely harsh!"],
+        ["A mysterious air current is protecting Flying Pokémon!"]
+    ];
+    //  TODO
+    ContinueMessage:[
+        [],
+        ["Rain continues to fail."],
+        ["The sunlight is strong."],
+        ["The sandstorm rages."],
+        ["Hail continues to fall."],
+        ["The heavy rain persists."],
+        ["The extremely harsh sunlight persists."],
+        ["The mysterious air current persists."]
+    ];
+    //  TODO =
+    EndMessage:[
+        [],
+        ["The rain stopped."],
+        ["The sunlight faded."],
+        ["The sandstorm subsided."],
+        ["The hail stopped."],
+        ["The heavy rain is lifted."],
+        ["The harsh sunlight faded."],
+        ["The mysterious air current dissipated."]
+    ];
+    //  TODO
+    DamageMessage:[
+        [],[],[],
+        [" is buffeted by the sand."],
+        [" is buffeted by the hail."],
+        [],[],[]
+    ];
+    //  TODO
+    PreventMoveMessage:[
+        [],[],[],[],[],
+        ["The Fire-type attack fizzled out in the heavy rain!"],
+        ["The Water-type attack evaporated in the harsh sunlight!"],
+        []
+    ];
+    //  TODO
+    PreventChangeMesage = [
+        [],[],[],[],[],
+        ["There is no relief from this heavy rain!"],
+        ["The extremely harsh sunlight was not lessened at all!"],
+        ["The mysterious air current blows regardless."]
+    ];
 ];
+
+module.exports = weather;
